26b994efb8adae51eda023d06a13edecb41adadc
Hiroki Osame <hiroki.osame@gmail.com>
2022-01-27 11:41:04 -0500

    refactor: esm

--- rollup.config.ts
+++ rollup.config.ts
@@ -1,18 +1,38 @@
+import { builtinModules } from 'module';
+import { defineConfig } from 'rollup';
 import esbuild from 'rollup-plugin-esbuild';
 import nodeResolve from '@rollup/plugin-node-resolve';
 import commonjs from '@rollup/plugin-commonjs';
+import dts from 'rollup-plugin-dts';
 
-export default {
-	input: 'src/index.ts',
-	plugins: [
-		nodeResolve(),
-		commonjs(),
-		esbuild({
-			minify: true,
-		}),
-	],
-	output: {
-		format: 'esm',
-		file: 'dist/index.mjs',
-	},
-};
+export default [
+	defineConfig({
+		input: 'src/index.ts',
+		plugins: [
+			nodeResolve(),
+			commonjs(),
+			esbuild({
+				minify: true,
+			}),
+		],
+		external: builtinModules,
+		output: [
+			{
+				format: 'esm',
+				file: 'dist/index.mjs',
+			},
+			{
+				format: 'cjs',
+				file: 'dist/index.js',
+			},
+		],
+	}),
+	defineConfig({
+		input: 'src/index.ts',
+		output: {
+			file: 'dist/indes.d.ts',
+			format: 'esm',
+		},
+		plugins: [dts()],
+	}),
+];
